553da10f9af200b37607c4436d18a2a9c8720cc0
GYHHY <gyhhy007@qq.com>
2019-01-05 17:17:01 +0800

    Add files via upload

--- src/cn/mcres/gyhhy/MXLib/bukkit/Plugin.java
+++ src/cn/mcres/gyhhy/MXLib/bukkit/Plugin.java
@@ -5,11 +5,13 @@
  */
 package cn.mcres.gyhhy.MXLib.bukkit;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import cn.mcres.gyhhy.MXLib.StringHelper;
 import cn.mcres.gyhhy.MXLib.http.WebHelper;
 import cn.mcres.gyhhy.MXLib.spigot.SpigotHelper;
+import cn.mcres.gyhhy.MXLib.log.Logger;
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -32,11 +34,14 @@ public class Plugin extends org.bukkit.plugin.java.JavaPlugin {
 
     public void onEnable() {
         Info info = Info.getInfo();
+        Map<String,Object> argc = new HashMap<>();
+        argc.put("","\u00a7e");
+        argc.put("b","\u00a7b");
         String sp
-                = String.format("MXLib Version: %s\nMineCraft version: %s\nFull version: %s\nNMS version: %s\nTitle API class path: %s\nSpigot: %s",
+                = StringHelper.variable(String.format("{}MXLib Version: {b}%s\n{}MineCraft version: {b}%s\n{}Full version: {b}%s\n{}NMS version: {b}%s\n{}Title API class path: {b}%s\n{}Spigot: {b}%s",
                         MXAPI.getVersion(),info.getServerMinecraftVersion(),
                         info.getFullVersion(), info.getServerNMSVersion(),
-                        MXAPI.getTitleAPI().getClass().getName(),SpigotHelper.isSupportSpigot());
+                        MXAPI.getTitleAPI().getClass().getName(),SpigotHelper.isSupportSpigot()),argc);
         write(sp);
         getServer().getScheduler().runTaskLaterAsynchronously(this, this::checkup, 0);
 //        checkup();
@@ -44,14 +49,14 @@ public class Plugin extends org.bukkit.plugin.java.JavaPlugin {
     }
 
     public void write(String sp) {
-        Logger logger = getLogger();
+        Logger logger = Logger.getOrCreateLogger(this);
         if (sp.indexOf('\n') > -1) {
             String[] lines = sp.split("\\n");
             for (String line : lines) {
-                logger.log(Level.INFO, line);
+                logger.printf(line);
             }
         } else {
-            logger.log(Level.INFO, sp);
+            logger.printf(sp);
         }
     }
 
@@ -84,7 +89,7 @@ public class Plugin extends org.bukkit.plugin.java.JavaPlugin {
         lgr.printf("this is a message with argc: {0} {1}", "arg0","arg1");
         PrintStream pr = lgr.getPrintStream();
         pr.println("This is a message with print stream.");
-        new Error().printStackTrace(lgr.getErrorPrintStream());
+        lgr.printStackTrace(new Error());
     }
 
     private void rundeb() {
--- src/cn/mcres/gyhhy/MXLib/log/Logger.java
+++ src/cn/mcres/gyhhy/MXLib/log/Logger.java
@@ -6,9 +6,16 @@
 package cn.mcres.gyhhy.MXLib.log;
 
 import cn.mcres.gyhhy.MXLib.StringHelper;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Scanner;
 import java.util.Locale;
 import java.util.Map;
+import java.util.logging.Level;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
@@ -143,13 +150,17 @@ public final class Logger {
         }
 
     }
-    public class ErrorPrintStream extends DefaultPrintStream{
-        private ErrorPrintStream(){}
-        public void print(String x){
+
+    public class ErrorPrintStream extends DefaultPrintStream {
+
+        private ErrorPrintStream() {
+        }
+
+        public void print(String x) {
             Logger.this.error(x);
         }
     }
-    
+
     public static Server server() {
         return Bukkit.getServer();
     }
@@ -158,9 +169,11 @@ public final class Logger {
         write(prefix + line);
         return this;
     }
-    public Logger printf(Object data){
+
+    public Logger printf(Object data) {
         return printf(String.valueOf(data));
     }
+
     public Logger printf(String line, Object... argc) {
         write(prefix + StringHelper.variable(line, argc));
         return this;
@@ -202,7 +215,12 @@ public final class Logger {
     }
 
     public static void write(String line) {
-        server().getConsoleSender().sendMessage(line);
+        Server server = server();
+        if (server == null) {
+            System.out.println(line);
+            return;
+        }
+        server.getConsoleSender().sendMessage(line);
     }
     private static final Object lock = new Object();
     private static Plugin[] px = new Plugin[0];
@@ -259,7 +277,8 @@ public final class Logger {
             lger = arr;
         }
     }
-    private DefaultPrintStream out,err;
+    private final DefaultPrintStream out, err;
+
     private Logger(Plugin plugin, String format, String errformat) {
         if (plugin == null) {
             throw new java.lang.IllegalArgumentException("No plugin found.");
@@ -273,16 +292,29 @@ public final class Logger {
         prefix = String.format(format, plugin.getName());
         errprefix = String.format(errformat, plugin.getName());
         this.plugin = plugin;
-        checkup(this, plugin);
+//        checkup(this, plugin);
         register(this);
         index = index(this);
         out = new DefaultPrintStream();
         err = new ErrorPrintStream();
     }
-    public PrintStream getPrintStream(){
+
+    public PrintStream getPrintStream() {
         return out;
     }
-    public PrintStream getErrorPrintStream(){return err;}
+
+    public PrintStream getErrorPrintStream() {
+        return err;
+    }
+
+    public static Logger getLogger(int id) {
+        synchronized (lock) {
+            if (id > -1 && id < lger.length) {
+                return lger[id];
+            }
+        }
+        return null;
+    }
 
     public static Logger getLogger(Plugin plugin) {
         if (plugin == null) {
@@ -333,5 +365,139 @@ public final class Logger {
     public static Logger createLogger(Plugin plugin, String format, String errorFormat) {
         return new Logger(plugin, format, errorFormat);
     }
+    private static final Method getOurStackTrace$met;
+    private static final Class<? extends ClassLoader> PluginClassLoaderClass;
+    private static final Field PluginClassLoader$plugin$field;
 
+    static {
+        Method met = null;
+        Field fie = null;
+        Class<? extends ClassLoader> cz = null;
+        try {
+            met = Throwable.class.getDeclaredMethod("getOurStackTrace");
+            cz = Class.forName("org.bukkit.plugin.java.PluginClassLoader").asSubclass(ClassLoader.class);
+            fie = cz.getDeclaredField("plugin");
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+        getOurStackTrace$met = met;
+        PluginClassLoaderClass = cz;
+        PluginClassLoader$plugin$field = fie;
+    }
+
+    private static StackTraceElement[] getOurStackTrace(Throwable thr) {
+        if (thr == null) {
+            return null;
+        }
+        try {
+            getOurStackTrace$met.setAccessible(true);
+            return (StackTraceElement[]) getOurStackTrace$met.invoke(thr);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+        return thr.getStackTrace();
+    }
+
+    private static Plugin getPluginClassLoaderPlugin(ClassLoader loader) {
+        if (!PluginClassLoaderClass.isInstance(loader)) {
+            throw new java.lang.ClassCastException("Cannot cast this class loader to org.bukkit.plugin.java.PluginClassLoader");
+        }
+        try {
+            PluginClassLoader$plugin$field.setAccessible(true);
+            return (Plugin) PluginClassLoader$plugin$field.get(loader);
+        } catch (RuntimeException ex) {
+            throw ex;
+        } catch (Error err) {
+            throw err;
+        } catch (Throwable thr) {
+            throw new RuntimeException(thr);
+        }
+    }
+
+    private void printStackTraceElement(StackTraceElement stack) {
+        String zip = null;
+        String version = null;
+        String clazz = stack.getClassName();
+        try {
+            Class<?> c = Class.forName(clazz);
+            URL url = c.getResource(c.getSimpleName() + ".class");
+            if (url != null) {
+                String toStr = url.toString();
+                if (toStr.startsWith("jar:file:")) {
+                    zip = toStr.replace("jar:file:", "").replaceFirst("!.*$", "").replaceFirst("^.*/", "");
+                } else if (toStr.startsWith("file:")) {
+                    zip = toStr.replace("file:", "").replaceFirst("." + clazz.replaceAll("\\$", ".") + ".class$", "").replaceFirst("^.*/", "");
+                }
+            }
+            ClassLoader loader = c.getClassLoader();
+            if (PluginClassLoaderClass.isInstance(loader)) {
+                version = getPluginClassLoaderPlugin(loader).getDescription().getVersion();
+            } else {
+                InputStream stream = c.getResourceAsStream("/META-INF/MANIFEST.MF");
+                if (stream != null) {
+                    Scanner scanner = new Scanner(stream);
+                    while (scanner.hasNextLine()) {
+                        String next = scanner.nextLine();
+                        if (next.startsWith("Implementation-Version:")) {
+                            version = next.replace("Implementation-Version:", "").trim();
+                            break;
+                        }
+                    }
+                    scanner.close();
+                    stream.close();
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+        } catch (Throwable thr) {
+            thr.printStackTrace();
+        }
+        errorformat("\t\u00a76at \u00a7c%s\u00a7f.\u00a7e%s\u00a7f(%s\u00a7f) [\u00a7b%s\u00a76:\u00a7d%s\u00a7f]",
+                clazz, stack.getMethodName(),
+                stack.isNativeMethod() ? "\u00a7dNative Method"
+                : (stack.getFileName() == null
+                ? "\u00a77Unknown Source"
+                : (stack.getLineNumber() > -1
+                ? "\u00a72" + stack.getFileName() + "\u00a7f:\u00a76" + stack.getLineNumber()
+                : "\u00a72" + stack.getFileName())),
+                zip == null ? "?" : zip,
+                version == null ? "?" : version);
+    }
+
+    public Logger printStackTrace(Throwable thr) {
+        if (thr == null) {
+            return this;
+        }
+        String clazz = thr.getClass().getName();
+        String msg = thr.getMessage();
+        StackTraceElement[] stackTrace = getOurStackTrace(thr);
+        synchronized (this) {
+            errorformat("\u00a7c%s\u00a7b: \u00a7e%s", clazz, msg);
+            for (StackTraceElement el : stackTrace) {
+                printStackTraceElement(el);
+            }
+        }
+        return this;
+    }
+
+    private Logger(String format, String errformat, String pname) {
+        if (format == null) {
+            format = "\u00a7f[\u00a7b%s\u00a7f] \u00a7e";
+        }
+        if (errformat == null) {
+            errformat = "\u00a7f[\u00a7b%s\u00a7f] \u00a7c";
+        }
+        prefix = String.format(format, pname);
+        errprefix = String.format(errformat, pname);
+        this.plugin = null;
+//        checkup(this, plugin);
+//        register(this);
+        index = -1;
+        out = new DefaultPrintStream();
+        err = new ErrorPrintStream();
+    }
+
+    public static void main(String[] args) {
+        Logger logger = new Logger((String) null, null, "Test");
+        logger.printStackTrace(new Error());
+    }
 }
