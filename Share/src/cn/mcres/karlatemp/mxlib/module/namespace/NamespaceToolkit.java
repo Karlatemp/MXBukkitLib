bae49b57ad544bf3152d77bc26a97b57056c5c79
Karlatemp <3279826484@qq.com>
2019-11-25 23:09:42 +0800

    2.7: Minecraft Key for ItemStack

---
+++ Share/src/cn/mcres/karlatemp/mxlib/module/namespace/NamespaceToolkit.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018-2019 Karlatemp. All rights reserved.
+ * Reserved.FileName: NamespaceToolkit.java@author: karlatemp@vip.qq.com: 19-11-24 下午9:51@version: 2.0
+ */
+
+package cn.mcres.karlatemp.mxlib.module.namespace;
+
+import cn.mcres.karlatemp.mxlib.annotations.Bean;
+import cn.mcres.karlatemp.mxlib.share.MinecraftKey;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The tools to get the MinecraftKey for Material.
+ *
+ * @since 2.7
+ */
+@Bean
+public interface NamespaceToolkit {
+    @NotNull
+    MinecraftKey getKey(@NotNull ItemStack stack);
+
+    @NotNull
+    MinecraftKey getKey(@NotNull Material material);
+
+    @NotNull
+    MinecraftKey getKey(@NotNull Material material, short data);
+}
